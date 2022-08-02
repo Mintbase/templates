9aa5de94c307a5618094ca729f1780f15c17301b
tony <antolisah@gmail.com>
2022-08-02 12:43:05 +0300

    ✨ simple marketplace

---
+++ simple-marketplace/pages/_app.tsx
@@ -0,0 +1,19 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NearWalletProvider } from "../services/providers/NearWalletProvider";
+import { ApolloProvider } from "@apollo/client";
+import { getClient } from "../services/providers/apollo";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const network = process.env.NEXT_PUBLIC_NETWORK || "testnet";
+
+  return (
+    <NearWalletProvider network={network}>
+      <ApolloProvider client={getClient({ network: network })}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </NearWalletProvider>
+  );
+}
+
+export default MyApp;
