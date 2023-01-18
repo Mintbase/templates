a98cf00df6b9d4d82598e7d4dd68f87941704f2c
ruisantiago <ruisantiagomr@gmail.com>
2023-01-18 11:01:49 +0000

    remove unused code

--- simple-marketplace/components/Item.tsx
+++ simple-marketplace/components/Item.tsx
@@ -15,57 +15,30 @@ function Item({
   const { mediaUrl } = parseMedia(item.media, item.base_uri);
 
   return (
-    <>
-      <MbThingCard
-        cardInfo={{
-          centerElement: (
-            <div className="w-full relative object-cover">
-              {mediaUrl ? (
-                <img
-                  src={getCachedImage(mediaUrl)}
-                  alt={item.title}
-                  className="rounded"
-                />
-              ) : (
-                <div className="w-full h-72 mb-10 flex justify-center items-center">
-                  {' '}
-                  No Nft Media Available
-                </div>
-              )}
-            </div>
-          ),
-          midLeftText: item.title,
-          midRightText: '',
-          botRightIcon: EIconName.NONE,
-          onCenterElementClick: () => showModal({ metadataId: item.metadata_id }),
-        }}
-      />
-      {/* <div className="bg-white rounded shadow-lg p-4 relative"> */}
-      {/* <div className="w-full h-72 mb-10 relative object-cover">
-        {mediaUrl ? (
-          <Image
-            src={getCachedImage(mediaUrl)}
-            alt={item.title}
-            layout="fill"
-          />
-        ) : (
-          <div className="w-full h-72 mb-10 flex justify-center items-center">
-            {" "}
-            No Nft Media Available
+    <MbThingCard
+      cardInfo={{
+        centerElement: (
+          <div className="w-full relative object-cover">
+            {mediaUrl ? (
+              <img
+                src={getCachedImage(mediaUrl)}
+                alt={item.title}
+                className="rounded"
+              />
+            ) : (
+              <div className="w-full h-72 mb-10 flex justify-center items-center">
+                {' '}
+                No Nft Media Available
+              </div>
+            )}
           </div>
-        )}
-      </div>
-      <div>
-        <div className="text-xl text-gray-800 font-bold">
-          {item.title ?? "No Nft Title"}
-        </div>
-        <div className="text-sm">{item.storeId}</div>
-      </div> */}
-      {/* <div className="flex items-center mt-2 justify-end">
-        <MbButton onClick={() => showModal(item)} label="BUY NOW" />
-      </div> */}
-      {/* </div> */}
-    </>
+        ),
+        midLeftText: item.title,
+        midRightText: '',
+        botRightIcon: EIconName.NONE,
+        onCenterElementClick: () => showModal({ metadataId: item.metadata_id }),
+      }}
+    />
   );
 }
 
