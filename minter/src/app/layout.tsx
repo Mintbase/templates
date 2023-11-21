01368ec6795d375d3084801947b1bba6eca09a6d
Ruben Marcus <ruben@rubenmarcus.dev>
2023-11-21 16:55:23 +0000

    new minter (layout & features) (#49)
    * minter
    
    * add card
    
    * fix minting and fix zod
    
    * fix storage issues
    
    * remove extra minter

---
+++ minter/src/app/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { MintbaseWalletContextProvider } from "@mintbase-js/react";
+import { MintbaseWalletSetup } from "@/config/setup";
+import "@near-wallet-selector/modal-ui/styles.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <MintbaseWalletContextProvider {...MintbaseWalletSetup}>
+      <html lang="en">
+        <body className={`${inter.className} dark`}>
+          <div className="flex flex-1 flex-col min-h-screen text-gray-500 gradient w-full  h-full flex justify-center items-center bold text-white">
+            {children}
+          </div>
+        </body>
+      </html>
+    </MintbaseWalletContextProvider>
+  );
+}
