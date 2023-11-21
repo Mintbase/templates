f07d0f5abb28f17c31041a2ce1fe54941dc8af11
Ruben Marcus <ruben@rubenmarcus.dev>
2023-11-21 16:51:54 +0000

    remove extra minter

--- minter/src/app/page.tsx
+++ minter/src/app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
                 </div>
                 <div>
                   <NearWalletConnector />
-                  <Minter />
                 </div>
               </div>
             </div>
