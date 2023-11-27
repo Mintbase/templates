3838f776070057582d5105d63009cad159f4db93
Luís Freitas <microchipgnu@gmail.com>
2023-11-27 10:51:59 +0000

    add credits

--- ai-chat/src/app/api/chat-completion/route.ts
+++ ai-chat/src/app/api/chat-completion/route.ts
@@ -6,18 +6,20 @@ export async function POST(req: Request) {
   const body = await req.json();
   const { messages } = body;
 
-  const response = await fetch("https://mintbase-wallet-jl7sxv687-mintbase.vercel.app/api/ai/v1/router/chat", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.MB_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "openai/gpt-4-1106-preview",
-      messages: messages,
-    }),
-  });
-
+  const response = await fetch(
+    "https://mintbase-wallet-git-ai-relayer-credits-mintbase.vercel.app/api/ai/v1/router/chat",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.MB_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "openai/gpt-4-1106-preview",
+        messages: messages,
+      }),
+    }
+  );
 
   return new StreamingTextResponse(response.body!);
 }
