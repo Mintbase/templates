c64bf526384edab036b1e154db8ec91cf34f948c
ruisantiago <ruisantiagomr@gmail.com>
2023-11-29 14:02:28 +0000

    + rm

--- simple-marketplace/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const parseMedia = (media: string, baseUri: string) => {
-  let mediaUrl = media?.indexOf('http') > -1 ? media : `${baseUri}/${media}`;
-
-  if (!media) {
-    mediaUrl = null;
-  }
-
-  return { mediaUrl };
-};
