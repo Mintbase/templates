572af6ba8ea8441cfcae36b5e5800e62c360375a
Rui Santiago <ruisantiagomr@gmail.com>
2023-12-05 12:02:39 +0000

    use network from .env files to get data (#71)

--- marketplace/src/hooks/useStoreNfts.ts
+++ marketplace/src/hooks/useStoreNfts.ts
@@ -5,32 +5,41 @@ Description: This hook calls storeNfts method from @mintbase-js/data to get stor
 
 */
 
-import { ParsedDataReturn, storeNfts } from '@mintbase-js/data';
-import { StoreNftsResult } from '@mintbase-js/data/lib/api/storeNfts/storeNfts.types';
-import { useQuery } from 'react-query';
-import { mbjs, NEAR_NETWORKS } from '@mintbase-js/sdk';
-import { MAINNET_CONFIG, TESTNET_CONFIG } from '../config/constants';
+import { ParsedDataReturn, storeNfts } from "@mintbase-js/data";
+import { StoreNftsResult } from "@mintbase-js/data/lib/api/storeNfts/storeNfts.types";
+import { useQuery } from "react-query";
+import { mbjs, NEAR_NETWORKS, Network } from "@mintbase-js/sdk";
+import { MAINNET_CONFIG, TESTNET_CONFIG } from "../config/constants";
 
 const mapStoreNfts = (data: ParsedDataReturn<StoreNftsResult>) => ({
   nftsData: data?.data?.mb_views_nft_metadata_unburned,
 });
 
 const useStoreNfts = (store?: string) => {
-  const stores = mbjs.keys?.network === NEAR_NETWORKS.TESTNET ? TESTNET_CONFIG.stores : MAINNET_CONFIG.stores;
+  const stores =
+    mbjs.keys?.network === NEAR_NETWORKS.TESTNET
+      ? TESTNET_CONFIG.stores
+      : MAINNET_CONFIG.stores;
 
   const defaultStores = process.env.NEXT_PUBLIC_STORES || stores;
 
   const formatedStores = defaultStores.split(/[ ,]+/);
 
-  const {
-    isLoading,
-    error,
-    data,
-  } = useQuery(['storeNfts', store], () => storeNfts(store || formatedStores, true), {
-    retry: false,
-    refetchOnWindowFocus: false,
-    select: mapStoreNfts,
-  });
+  const { isLoading, error, data } = useQuery(
+    ["storeNfts", store],
+    () =>
+      storeNfts(
+        store || formatedStores,
+        true,
+        undefined,
+        (process?.env?.NEXT_PUBLIC_NETWORK as Network) || "testnet"
+      ),
+    {
+      retry: false,
+      refetchOnWindowFocus: false,
+      select: mapStoreNfts,
+    }
+  );
 
   return { ...data, error, loading: isLoading };
 };
