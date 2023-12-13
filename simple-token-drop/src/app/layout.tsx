208ba98698f213e4d1fac58ca9010de6a57f06fa
Sérgio <sergioseco98@gmail.com>
2023-12-13 18:22:38 +0000

    Changed metadata and removed unused code

--- simple-token-drop/src/app/layout.tsx
+++ simple-token-drop/src/app/layout.tsx
@@ -6,15 +6,8 @@ import "@near-wallet-selector/modal-ui/styles.css";
 import "../styles.css";
 
 
-import { MintbaseWalletContextProvider } from "@mintbase-js/react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const MintbaseWalletSetup = {
-  contractAddress: "hellovirtualworld.mintspace2.testnet",
-  network: "testnet" as any,
-  callbackUrl: "http://localhost:3000",
-};
 
 export default function RootLayout({
   children,
@@ -22,14 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <MintbaseWalletContextProvider onlyMbWallet={true} {...MintbaseWalletSetup}>
-      <html lang="en">
-        <body className={inter.className}>
-          <div className="flex flex-1 flex-col min-h-screen text-gray-500 gradient w-full  h-full flex justify-center items-center bold text-white">
-            {children}
-          </div>
-        </body>
-      </html>
-    </MintbaseWalletContextProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex flex-1 flex-col min-h-screen text-gray-500 gradient w-full  h-full flex justify-center items-center bold text-white">
+          {children}
+        </div>
+      </body>
+    </html>
   );
 }
