d77e859136d325f32751be69d0fa70263c2d9ff8
ruisantiago <ruisantiagomr@gmail.com>
2024-01-25 11:59:06 +0000

    Update Card UI + Add .env.example

--- marketplace/src/components/Item.tsx
+++ marketplace/src/components/Item.tsx
@@ -1,8 +1,8 @@
-import { EIconName, MbThingCard } from "mintbase-ui";
+import { bigToNear, yoctoToNear } from "@/utils/numbers";
+import { StoreNftsData } from "@mintbase-js/data/lib/api/storeNfts/storeNfts.types";
 import { SelectedNft } from "../types/types";
 import { parseMedia } from "../utils";
 import { getCachedImage } from "../utils/getCachedImages";
-import { StoreNftsData } from "@mintbase-js/data/lib/api/storeNfts/storeNfts.types";
 
 function Item({
   item,
@@ -11,33 +11,37 @@ function Item({
   item: StoreNftsData;
   showModal: (item: SelectedNft) => void;
 }): JSX.Element {
-  const { mediaUrl } = parseMedia(item.media, item.base_uri);
+  if (!item) {
+    return <></>;
+  }
+
+  const { base_uri, media, metadata_id, price, title } = item;
+
+  const { mediaUrl } = parseMedia(media, base_uri);
 
   return (
-    <MbThingCard
-      cardInfo={{
-        centerElement: (
-          <div className="w-full relative object-cover">
-            {mediaUrl ? (
-              <img
-                src={getCachedImage(mediaUrl)}
-                alt={item.title}
-                className="rounded"
-              />
-            ) : (
-              <div className="w-full h-72 mb-10 flex justify-center items-center">
-                {" "}
-                No Nft Media Available
-              </div>
-            )}
+    <div
+      className="p-2 bg-black bg-opacity-10 hover:bg-opacity-20 transition-all duration-300 rounded-xl shadow-xl cursor-pointer"
+      onClick={() => showModal({ metadataId: metadata_id })}
+    >
+      <div className="w-full relative object-cover">
+        {mediaUrl ? (
+          <img
+            src={getCachedImage(mediaUrl)}
+            alt={title}
+            className="rounded-md"
+          />
+        ) : (
+          <div className="w-full h-72 mb-10 flex justify-center items-center">
+            No Nft Media Available
           </div>
-        ),
-        midLeftText: item.title,
-        midRightText: "",
-        botRightIcon: EIconName.NONE,
-        onCenterElementClick: () => showModal({ metadataId: item.metadata_id }),
-      }}
-    />
+        )}
+      </div>
+      <div className="flex flex-col mt-2">
+        <div className="font-semibold text-md">{title}</div>
+        <div className="text-xs">{bigToNear(price?.toString() || "0")} N</div>
+      </div>
+    </div>
   );
 }
 
@@ -48,7 +52,7 @@ function LoadingItem(): JSX.Element {
     <>
       {products.map((productKey) => (
         <div key={productKey} className="flex items-center justify-center ">
-          <div className="w-full h-64 bg-slate-900 animate-pulse" />
+          <div className="w-full h-72 bg-slate-900 animate-pulse rounded-xl shadow-xl" />
         </div>
       ))}
     </>
