abc1f9ba8dea261c2e90141b4540f5fc461d27be
Ruben Marcus <ruben@rubenmarcus.dev>
2024-02-15 16:18:05 +0000

    fix marketplace

--- marketplace/src/app/layout.tsx
+++ marketplace/src/app/layout.tsx
@@ -8,14 +8,25 @@ import "./globals.css";
 import { MintbaseWalletContextProvider } from "@mintbase-js/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Header from "@/components/header";
+import { mbjs } from "@mintbase-js/sdk";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const MintbaseWalletSetup = {
-  contractAddress: "hellovirtualworld.mintspace2.testnet",
-  network: "testnet",
-  callbackUrl: "http://localhost:3000",
-};
+export const isDev = process.env.NEXT_PUBLIC_ENV === 'dev'
+
+export const getCallbackUrl = () => {
+  let callbackUrl = ''
+
+  if (typeof window !== 'undefined') {
+    callbackUrl =
+      isDev || window?.location?.host.includes('localhost')
+        ? `http://${window?.location.host}`
+        : `}`
+  }
+
+  return callbackUrl
+}
+
 
 export default function RootLayout({
   children,
@@ -23,6 +34,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const queryClient = new QueryClient();
+
+  mbjs.config({
+    network: process.env.NEXT_PUBLIC_NETWORK || 'testnet'
+  })
+
+  const MintbaseWalletSetup = {
+    contractAddress: "hellovirtualworld.mintspace2.testnet",
+    network: "testnet",
+    callbackUrl: getCallbackUrl(),
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
