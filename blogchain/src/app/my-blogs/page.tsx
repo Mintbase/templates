e184ea05db9a813d3a32818f67bcf8958a011b52
ruisantiago <ruisantiagomr@gmail.com>
2024-02-19 20:45:59 +0000

    fix detail pages gap

--- blogchain/src/app/my-blogs/page.tsx
+++ blogchain/src/app/my-blogs/page.tsx
@@ -5,7 +5,7 @@ import UserBlogs from "@/components/user-blogs";
 
 export default function MyBlogs() {
   return (
-    <main className="flex min-h-screen flex-col items-start gap-4 mt-12 px-12">
+    <main className="flex min-h-screen flex-col items-start gap-4 px-2 lg:px-24 py-12">
       <CreateBlogDialog />
       <UserBlogs />
     </main>
