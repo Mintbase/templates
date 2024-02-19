53568827630f1e7e1e20c8da24fbf7fe6628eca7
Ruben Marcus <ruben@rubenmarcus.dev>
2024-02-19 13:25:41 +0000

    update minter

--- minter/src/hooks/useMint.ts
+++ minter/src/hooks/useMint.ts
@@ -7,7 +7,7 @@
  * The hook returns an object containing the form handlers and a preview state for the image to be minted.
  */
 
-"use client"
+"use client";
 
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -16,10 +16,14 @@ import { useMbWallet } from "@mintbase-js/react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { ArweaveResponse, uploadFile, uploadReference } from "@mintbase-js/storage"
+import {
+  ArweaveResponse,
+  uploadFile,
+  uploadReference,
+} from "@mintbase-js/storage";
 import { formSchema } from "./formSchema";
 import { MintbaseWalletSetup, proxyAddress } from "@/config/setup";
-import { Wallet } from "@near-wallet-selector/core"
+import { Wallet } from "@near-wallet-selector/core";
 import { cbUrl } from "./utils";
 
 interface SubmitData {
@@ -27,7 +31,6 @@ interface SubmitData {
   description: string;
   media: ((false | File) & (false | File | undefined)) | null;
 }
-
 
 const useMintImage = () => {
   const { selector, activeAccountId } = useMbWallet();
@@ -42,25 +45,30 @@ const useMintImage = () => {
     }
   };
 
-
   const onSubmit = async (data: SubmitData) => {
     const wallet = await getWallet();
 
     const reference = await uploadReference({
-      title: typeof data?.title === 'string' ? data.title : '',
-      media: data?.media as unknown as File
-    })
+      title: typeof data?.title === "string" ? data.title : "",
+      media: data?.media as unknown as File,
+    });
 
-    console.log(reference, 'reference')
+    console.log(reference, "reference");
 
-    const file = uploadFile(data?.media as unknown as File
+    const file = uploadFile(data?.media as unknown as File);
+
+    await handleMint(
+      reference.id,
+      file,
+      activeAccountId as string,
+      wallet,
+      reference.media_url as string,
+      data.title
     );
-
-    await handleMint(reference.id, file, activeAccountId as string, wallet, reference.media_url as string, data.title);
   };
 
   const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema)
+    resolver: zodResolver(formSchema),
   });
 
   async function handleMint(
@@ -69,32 +77,32 @@ const useMintImage = () => {
     activeAccountId: string,
     wallet: Wallet,
     mediaUrl: string,
-    nftTitle: string,
+    nftTitle: string
   ) {
-
-
     const callbackArgs = {
       contractAddress: MintbaseWalletSetup.contractAddress.toString(),
       amount: 1,
       ref: `${reference}`,
       mediaUrl: mediaUrl,
-      title: nftTitle
-    }
-
+      title: nftTitle,
+    };
 
     if (reference) {
       await wallet.signAndSendTransaction({
         signerId: activeAccountId,
         receiverId: proxyAddress,
-        callbackUrl:cbUrl(reference, callbackArgs),
+        callbackUrl: cbUrl(reference, callbackArgs),
         actions: [
           {
             type: "FunctionCall",
             params: {
               methodName: "mint",
               args: {
-                metadata: JSON.stringify({ reference, media: (await media).id }),
-                 nft_contract_id: MintbaseWalletSetup.contractAddress,
+                metadata: JSON.stringify({
+                  reference,
+                  media: (await media).id,
+                }),
+                nft_contract_id: MintbaseWalletSetup.contractAddress,
               },
               gas: "200000000000000",
               deposit: "10000000000000000000000",
