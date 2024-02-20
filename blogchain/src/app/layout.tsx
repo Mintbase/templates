8878f53c8973ba9b4d1e594f8a3db6e44028c69f
ruisantiago <ruisantiagomr@gmail.com>
2024-02-20 10:59:20 +0000

    metadata

--- blogchain/src/app/layout.tsx
+++ blogchain/src/app/layout.tsx
@@ -10,7 +10,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: `Blogchain`,
-  description: "The decentralized writer's blog",
+  description: "The decentralized writers blog",
+  openGraph: {
+    title: `Blogchain`,
+    description: "Your forever thoughts on the Blockchain",
+    images: [
+      {
+        type: "image/png",
+        url: "./thumbnail.png",
+        width: "1200",
+        height: "630",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `Blogchain`,
+    description: "Your forever thoughts on the Blockchain",
+    creator: "Mintbase",
+    images: "./thumbnail.png",
+  },
 };
 
 export default function RootLayout({
