de88e80bd52bd4dfa10db960ce55627b6b173d36
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-09-10 15:52:03 +0200

    request parsing

--- cowswap-ai-plugin/app/quote/route.ts
+++ cowswap-ai-plugin/app/quote/route.ts
@@ -1,35 +1,27 @@
 
-import { CowNetwork } from "@/utils/tokens";
+import { parseQuoteRequest } from "@/utils/tokens";
 import { type NextRequest, NextResponse } from "next/server";
 
 const COW_API = "https://api.cow.fi"
 
-
-    
-
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  const requestBody = await req.json();
-  console.log("POST Request for quote:", requestBody)
+  try {
+    const requestBody = await parseQuoteRequest(req);
+    console.log("POST Request for quote:", requestBody)
+    
+    const response = await fetch(`${COW_API}/${requestBody.network}/api/v1/quote`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody)
+    });
+    if (!response.ok) {
+      const message = await response.text();
+      return NextResponse.json({ message }, { status: response.status });
+    }
+    const data = await response.json();
 
-  const network: CowNetwork = requestBody.network;
-  
-  if (!["mainnet", "xdai", "arbitrum_one"].includes(network)) {
-    return NextResponse.json({ error: `Invalid network '${network}'. Must be one of 'mainnet', 'xdai' OR 'arbitrum_one'` }, { status: 400 });
+    return NextResponse.json(data);
+  } catch (error: unknown) {
+    return NextResponse.json({ error }, { status: 400 });
   }
-
-  const response = await fetch(`${COW_API}/${network}/api/v1/quote`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(requestBody)
-  });
-  if (!response.ok) {
-    const message = await response.text();
-    console.error(message)
-    throw new Error(message)
-  }
-  const data = await response.json();
-
-  return NextResponse.json(data);
 }
--- cowswap-ai-plugin/utils/tokens.ts
+++ cowswap-ai-plugin/utils/tokens.ts
@@ -1,8 +1,11 @@
 import * as fs from 'fs';
 import csv from 'csv-parser';
+import { Address, isAddress, parseUnits } from 'viem';
+import { Network, setupAdapter } from 'near-ca';
+import { NextRequest } from 'next/server';
 
 interface TokenInfo {
-    address: string;
+    address: Address;
     decimals: number;
 }
 
@@ -42,18 +45,90 @@ export async function loadTokenMapping(filePath: string): Promise<BlockchainMapp
 
 export type CowNetwork = "mainnet" | "xdai" | "arbitrum_one";
 
-type DuneNetwork = "ethereum" | "gnosis" | "arbitrum";
+// type DuneNetwork = "ethereum" | "gnosis" | "arbitrum";
+let tokenMap: BlockchainMapping;
 
-const cowToDuneMapping: Record<CowNetwork, DuneNetwork> = {
-  mainnet: "ethereum",
-  xdai: "gnosis",
-  arbitrum_one: "arbitrum"
+const chainIdFromNetwork: Record<CowNetwork, number> = {
+  mainnet: 1,
+  xdai: 100,
+  arbitrum_one: 42161
 };
 
-export async function tokenFromSymbol(network: CowNetwork, symbol: string): Promise<TokenInfo> {
+export async function getTokenDetails(network: CowNetwork, symbolOrAddress: string): Promise<TokenInfo> {
+  if (isAddress(symbolOrAddress)) {
+    return {
+      address: symbolOrAddress as Address,
+      decimals: await getTokenDecimals(chainIdFromNetwork[network], symbolOrAddress)
+    };
+  }
+  console.log("Seeking TokenMap for Symbol -> Address conversion")
   // TODO. Load once and cache.
   // Token data comes from https://dune.com/queries/4055949
   //  curl -X GET https://api.dune.com/api/v1/query/4055949/results/csv -H "x-dune-api-key: $DUNE_API_KEY"  > tokens.csv
-  const tokenMap = await loadTokenMapping("./tokens.csv")
-  return tokenMap[cowToDuneMapping[network]!][symbol]
+  if (!tokenMap) {
+    // half-ass attempt to load to memory.
+    tokenMap = await loadTokenMapping("./tokens.csv")
+  }
+  return tokenMap[network][symbolOrAddress]
+}
+
+// Function to request token decimals
+async function getTokenDecimals(chainId: number, tokenAddress: string): Promise<number> {
+  const client = Network.fromChainId(chainId).client;
+  try {
+    const decimals = await client.readContract({
+      address: tokenAddress as `0x${string}`,
+      abi: ['function decimals() view returns (uint8)'],
+      functionName: 'decimals',
+    });
+
+    return decimals as number;
+  } catch (error: unknown) {
+    throw new Error(`Error fetching token decimals: ${error}`);
+  }
+}
+
+export interface QuoteRequestBody {
+  sellToken: Address;
+  buyToken: Address;
+  sellAmountBeforeFee: string;
+  kind: "buy" | "sell";
+  receiver: Address,
+  from: Address,
+  // network is not officially part of the COW API expectations
+  network: CowNetwork
+}
+
+export async function parseQuoteRequest(req: NextRequest): Promise<QuoteRequestBody> {
+  // TODO - Add Type Guard on Request (to determine better if it needs processing below.)
+  const requestBody = await req.json();
+
+  const {sellToken, buyToken, network, sellAmountBeforeFee, from} = requestBody;
+
+  if (!["mainnet", "xdai", "arbitrum_one"].includes(network)) {
+    throw new Error(`Invalid network '${network}'. Must be one of 'mainnet', 'xdai' OR 'arbitrum_one'`);
+  }
+
+  const [buyTokenData, sellTokenData] = await Promise.all([
+    getTokenDetails(network, buyToken),
+    getTokenDetails(network, sellToken),
+  ]);
+
+  let sender = from;
+  if (!isAddress(from)) {
+    console.log(`Transforming near address ${from} to EVM address`)
+    const adapter = await setupAdapter({accountId: from, mpcContractId: "v1.signer"});
+    sender = adapter.address;
+  }
+
+  return {
+    sellToken: sellTokenData.address,
+    buyToken: buyTokenData.address,
+    sellAmountBeforeFee: parseUnits(sellAmountBeforeFee, sellTokenData.decimals).toString(),
+    kind: "sell",
+    // TODO - change this when we want to enable alternate recipients.
+    receiver: sender,
+    from: sender,
+    network: requestBody.network
+  }
 }
