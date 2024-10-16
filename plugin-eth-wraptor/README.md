da7eb7a09ec2721a3dc5b930613146c3eb9f89ca
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-10-16 15:20:15 +0200

    Scaffold ETH Wraptor

---
+++ plugin-eth-wraptor/README.md
@@ -0,0 +1,7 @@
+# ETH Wraptor
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSg_Qb_8YmjKCbO3bdXt0QfnZnCtKUWZ4owgU1ugQoSdFPkbcL9gkkFQTabM4VfiOudT8&usqp=CAU" alt="cover_image" width="0" />
+
+## Description
+
+Wrap and unwrap ETH from a chat prompt.
---
+++ plugin-eth-wraptor/app/route.ts
@@ -0,0 +1,27 @@
+import type { ReferenceConfiguration } from "@scalar/api-reference";
+import { ApiReference } from "@scalar/nextjs-api-reference";
+import { DEPLOYMENT_URL } from "vercel-url";
+
+const config: ReferenceConfiguration = {
+  spec: {
+    url: "/.well-known/ai-plugin.json",
+  },
+  theme: "moon",
+  metaData: {
+    title: "ETH Wraptor AI Plugin",
+    description: "Bitte.ai Plugin for Wrappin' ETH",
+    ogDescription: "AI Compatible API endpoints Wrapped ETH",
+    ogTitle: "Wraptor",
+    ogImage: "https://img.cryptorank.io/coins/weth1701090834118.png",
+    twitterCard: "summary_large_image",
+  },
+  servers: [
+    {
+      // Override server URLs using Vercel system env variables
+      // Defaults to http://localhost:3000 on local development
+      url: `${DEPLOYMENT_URL}`,
+    }
+  ]
+};
+
+export const GET = ApiReference(config);
