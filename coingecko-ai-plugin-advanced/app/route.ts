b34220d0cc52d20e12c542ff374f40ff5902043b
Markeljan Sokoli <Markeljan19@gmail.com>
2024-11-13 20:05:46 -0500

    Next 15 , preview images, deps, biomejs

--- coingecko-ai-plugin-advanced/app/route.ts
+++ coingecko-ai-plugin-advanced/app/route.ts
@@ -1,27 +1,37 @@
-import type { ReferenceConfiguration } from "@scalar/api-reference";
-import { ApiReference } from "@scalar/nextjs-api-reference";
+import {
+	ApiReference,
+	type ApiReferenceOptions,
+} from "@scalar/nextjs-api-reference";
 import { DEPLOYMENT_URL } from "vercel-url";
 
-const config: ReferenceConfiguration = {
-  spec: {
-    url: "/.well-known/ai-plugin.json",
-  },
-  theme: "purple",
-  metaData: {
-    title: "CoinGecko AI Plugin",
-    description: "Bitte.ai Plugin for CoinGecko API's",
-    ogDescription: "AI Compatible API endpoints for Prices, Charts, and more.",
-    ogTitle: "CoinGecko AI",
-    ogImage: "https://lvjt7wkmlmpwhrpm.public.blob.vercel-storage.com/Screenshot%202024-07-31%20at%202.11.34%E2%80%AFPM-9TeD4oYi8W1jhTDwEhuy0c6d3vQeM1.png",
-    twitterCard: "summary_large_image",
-  },
-  servers: [
-    {
-      // Override server URLs using Vercel system env variables
-      // Defaults to http://localhost:3000 on local development
-      url: `${DEPLOYMENT_URL}/api/v3`,
-    }
-  ]
+const config: ApiReferenceOptions = {
+	spec: {
+		url: "/.well-known/ai-plugin.json",
+	},
+	theme: "purple",
+	metaData: {
+		title: "CoinGecko AI Plugin",
+		description: "Bitte.ai Plugin for CoinGecko API's",
+		ogDescription: "AI Compatible API endpoints for Prices, Charts, and more.",
+		ogTitle: "CoinGecko Agent",
+		ogImage: {
+			url: `${DEPLOYMENT_URL}/opengraph-image.png`,
+			type: "image/png",
+			alt: "CoinGecko AI Plugin API Reference",
+			width: 1200,
+			height: 630,
+		},
+		twitterCard: "summary_large_image",
+		twitterSite: "@bitteprotocol",
+		twitterImage: {
+			url: `${DEPLOYMENT_URL}/twitter-image.png`,
+			type: "image/png",
+			alt: "CoinGecko AI Plugin API Reference",
+			width: 1200,
+			height: 600,
+		},
+	},
+	favicon: "/favicon.ico",
 };
 
 export const GET = ApiReference(config);
